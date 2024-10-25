1693e0be8d47ff6c93a14f5026d5af7985b25ea2
sadulla018 <sadulla499@gmail.com>
2024-10-25 12:53:45 +0530

    commit

---
+++ conditions.js
@@ -0,0 +1,61 @@
+var ligth="red";
+if(ligth=="red"){
+    console.log("Stop");}
+ else if(ligth=="yellow"){
+        console.log("Get ready to move");}
+ else if(ligth=="green"){
+            console.log("Go");
+        }
+else{
+    console.log("Invalid input");}
+
+    //                     task2
+
+var temp=0;
+if(temp==0){
+    console.log("It's freezing cold");
+}
+else if(temp==15){
+    console.log("It's a cool day");
+}
+else if(temp==25){
+    console.log("It's a pleasent day");
+
+}
+else{
+    console.log("Unkonwn weather");
+}
+
+
+
+
+
+
+//               Task3    //
+
+
+var day=1;
+if(day==1){
+    console.log("Monday");
+}
+else if(day==2){
+    console.log("Tuesday");
+}
+else if(day==3){
+    console.log("Wednesday");
+}
+else if(day==4){
+    console.log("Thrusday");
+}
+else if(day==5){
+    console.log("Friday");
+}
+else if(day==6){
+    console.log("Saturday");
+}
+else if(day==7){
+    console.log("sunday");
+}
+else{
+    console.log("Invakid day number");
+}
---
+++ js1.js
@@ -0,0 +1,19 @@
+var fruits = ['Apple', 'Banana', 'Orange'];
+console.log(fruits.join())
+var numbers = [1,2,3,4,5];
+console.log(numbers.join('-'));
+var chars = ['H', 'e', 'l', 'l', 'o'];
+console.log(chars.join(''))
+var text = 'JavaScript is awesome';
+console.log(text.slice(0,10))
+var sentence = 'I love learning JavaScript!';
+console.log(sentence.slice(7,15))
+var text = 'Fronted Development';
+console.log(text.slice(-11))
+var date = '2024-10-21';
+console.log(date.split("-"))
+var fruits = ['Apple', 'Banana'];
+c = fruits.join()
+console.log('"'+c+'"')
+var text = 'I love Javascript programming';
+console.log(text.split())
